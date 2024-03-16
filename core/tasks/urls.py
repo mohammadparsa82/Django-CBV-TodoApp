37f59998e32eecd9b5d08977a446418affd7754a
mohammad <121767738+mohammadparsa82@users.noreply.github.com>
2024-03-15 18:32:48 -0700

    commit

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path , include
 from tasks.views import *
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("update/<int:pk>/", UpdateTask.as_view(), name="update_task"),
     path("delete/<int:pk>/", DeleteTask.as_view(), name="delete_task"),
     path("complete/<int:pk>/", TaskComplete.as_view(), name="complete_task"),
+    path('api/v1/',include('tasks.api.v1.urls')),
 ]
